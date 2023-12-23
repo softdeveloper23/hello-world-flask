86fc4d2390f76d174dd9686334b060b8c9519948
unknown <brannongarrett77@gmail.com>
2023-12-23 14:01:20 +0100

    Updated the code and added an input form

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route('/greet')
+def greet():
+    name = request.args.get("name", "World")
+    return render_template("greet.html", name=name)
